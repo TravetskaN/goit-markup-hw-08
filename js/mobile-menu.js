c2c1bc1e990f31fe4e97f7ecd57b686ccfb4b202
TravetskaN <ntravetskaya@gmail.com>
2021-11-30 18:57:31 +0200

    ДЗ

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-mobile-menu-open]"),
-    closeModalBtn: document.querySelector("[data-mobile-menu-close]"),
-    modal: document.querySelector("[data-mobile-menu]"),
-  };
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-visible");
-  }
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
